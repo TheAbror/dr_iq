bc3bd7944b71e4eaba50b17c5ecf49c02e932524
Elbarto <oyjddkvj@gmail.com>
2023-11-14 12:14:59 +0500

    ca

--- lib/core/roots/main_route_generator.dart
+++ lib/core/roots/main_route_generator.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:dr_iq/core/roots/app_routes.dart';
 import 'package:dr_iq/core/roots/custom_cupertino_route.dart';
 import 'package:dr_iq/ui/home_page/home_page.dart';
-import 'package:dr_iq/ui/home_page/profile_page/profile_page.dart';
+import 'package:dr_iq/ui/home_page/tabs/history_page/profile_tab/profile_page.dart';
 import 'package:dr_iq/ui/home_page/tabs/history_page/history_page.dart';
 import 'package:dr_iq/ui/home_page/tabs/take_iq_test_tab/take_iq_test_page.dart';
 import 'package:dr_iq/ui/sign_in_page/sign_in_page.dart';
